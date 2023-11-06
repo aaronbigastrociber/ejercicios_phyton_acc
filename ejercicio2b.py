7a10226154ab69fe08d215716a500b1929112e4a
Aaroón Caselles Casanova <aarcascas2@alu.edu.gva.es>
2023-11-06 19:15:49 +0100

    ejercicios 2, 2b y 3

--- ejercicio2b.py
+++ ejercicio2b.py
@@ -2,15 +2,24 @@
 #Pide la edad y el nombre por teclado. En caso de ser mayor de 18 años indica que ya puede conducir.
 
 # Implemente función esMayorEdad(e)
-
+def esMayorEdad(e):
+    if e>=18 :
+        return True
+    else:
+        return False
+    
 
 # Programa principal
 def main():
     nombre="Mariano";
-    edad=0;
+    edad=19;
 
     # Utilice la función definida
     # Estructura alternativa Si (condidición con función) entonces --> sino ...
+    if esMayorEdad(edad):
+        print("Usted es mayor de edad")
+    else:
+        print("Todavia eres menor de edad")
 
 if __name__== "__main__" :
    main()
--- ejercicio3.py
+++ ejercicio3.py
@@ -15,6 +15,18 @@ __Author__="José Gaspar Sánchez García"
 def obtenerCalificaion(nota) :
     calificacion="Incorrecta"
    # Implemente aquí ... Si (condición) entonces ... sino ... si (condición) entonces ... sino ...
+    if nota>=0 and nota<3:
+        calificacion="Muy deficiente"
+    elif nota>=3 and nota<5:
+        calificacion="Insuficiente"
+    elif nota>=5 and nota<6:
+        calificacion="Suficiente"
+    elif nota>=6 and nota<7:
+        calificacion="Bien"
+    elif nota>=7 and nota<9:
+        calificacion="Notable"
+    elif nota>=9 and nota<=10:
+        calificacion="Sobresaliente"       
     return calificacion
 
 # Programa principal
